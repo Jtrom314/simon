93ed7d03f755c4f5f9f9c295b352b38155fc813c
Jtrom314 <jtrom2021@gmail.com>
2023-01-24 12:58:37 -0700

    Updated scores.js

--- scores.js
+++ scores.js
@@ -1,9 +1,20 @@
-function loadScores() {
+async function loadScores() {
     let scores = [];
-    const scoresText = localStorage.getItem('scores');
-    if (scoresText) {
-        scores = JSON.parse(scoresText);
+    try {
+        const response = await fetch('/api/scores');
+        scores = await response.json();
     }
+    catch {
+        const scoresText = localStorage.getItem('scores');
+        if (scoresText) {
+            scores = JSON.parse(scoresText);
+        }
+    }
+    displayScores(scores);
+}
+
+function displayScores(scores) {
+    let scores = [];
     const tableBodyEl = document.querySelector('#scores');
     if (scores.length) {
         for (const [i, score] of scores.entries()) {
