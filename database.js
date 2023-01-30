8072c15a0d801621ae8ba672af9ba535dd4793d6
Jtrom314 <jtrom2021@gmail.com>
2023-01-30 11:19:49 -0700

    Bug Tested

--- database.js
+++ database.js
@@ -7,55 +7,55 @@ const password = process.env.MONGOPASSWORD;
 const hostname = process.env.MONGOHOSTNAME;
 
 if (!userName) {
-    throw Error('Database not configured. Set environment variables');
+  throw Error('Database not configured. Set environment variables');
 }
 
-
 const url = `mongodb+srv://${userName}:${password}@${hostname}`;
-console.log("URL:  " + url);
 
-const client = new MongoClient(url, {useUnifiedTopology: true });
-const scoreCollection = client.db('simon').collection('score');
+const client = new MongoClient(url);
 const userCollection = client.db('simon').collection('user');
+const scoreCollection = client.db('simon').collection('score');
 
 function getUser(email) {
-    return userCollection.findOne({ email: email});
+  return userCollection.findOne({ email: email });
 }
 
 function getUserByToken(token) {
-    return userCollection.findOne({ token: token });
+  return userCollection.findOne({ token: token });
 }
 
 async function createUser(email, password) {
-    const passwordHash = await bcrypt.hash(password, 10);
-    const user = {
-        email: email,
-        password: passwordHash,
-        token: uuid.v4(),
-    };
-    await userCollection.insertOne(user);
-    return user;
+  // Hash the password before we insert it into the database
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = {
+    email: email,
+    password: passwordHash,
+    token: uuid.v4(),
+  };
+  await userCollection.insertOne(user);
+
+  return user;
 }
 
- function addScore(score) {
-    console.log("IN ADD SCORE");
-    scoreCollection.insertOne(score);
+function addScore(score) {
+  scoreCollection.insertOne(score);
 }
 
- function getHighScores(score) {
-    const query = {};
-    const options = {
-        sort: { score: -1},
-        limit: 10,
-    };
-    const cursor = scoreCollection.find(query, options);
-    return cursor.toArray();
+function getHighScores() {
+  const query = {};
+  const options = {
+    sort: { score: -1 },
+    limit: 10,
+  };
+  const cursor = scoreCollection.find(query, options);
+  return cursor.toArray();
 }
 
-module.exports = { 
-    addScore, 
-    getHighScores,
-    getUser,
-    getUserByToken,
-    createUser,    
+module.exports = {
+  getUser,
+  getUserByToken,
+  createUser,
+  addScore,
+  getHighScores,
 };
